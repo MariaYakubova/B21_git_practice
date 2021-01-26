8a2eeef202d68fae1e3e8a6aa694a04cccf927f8
Maria Yakubova <mariaaliceyakubova@gmail.com>
2021-01-26 14:25:00 -0600

    More work

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello B21");
         System.out.println("Work hard");
+        System.out.println("more work");
     }
 }
